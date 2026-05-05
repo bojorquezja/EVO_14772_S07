9947806cd7bcd13b3da756a7ef8d222ff233fc88
bojorquezja <bojorquez.ja@gmail.com>
2026-05-05 16:57:00 -0500

    email notification agregado

--- DIP/Program.cs
+++ DIP/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DIP;
+
+var orderProcessor = new OrderProcessor();
+orderProcessor.ProcessOrder();
